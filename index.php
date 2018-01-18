6da100093012150ec76bfedeeefc24483aefe881
aurelien-blot <aurelien.blot@gmail.com>
2018-01-18 10:25:46 +0100

    ajout code base html

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Championnat de babyfoot</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+    </head>
+    <body>
+        <header>
 
+        </header>
+
+        <div id="content">
+
+        </div>
+        <footer>
+
+        </footer>
+    </body>
+</html>
