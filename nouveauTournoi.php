b7208b5537a9dba606aab26866c754966fd88dc7
aurelien-blot <aurelien.blot@gmail.com>
2018-01-19 10:01:41 +0100

    php + cahampionnat

--- nouveauTournoi.php
+++ nouveauTournoi.php
@@ -21,12 +21,29 @@ include'include/connexionBdd.php';
         <nav>
             <?php include'include/navChamp.php'; ?>
         </nav>
-        <form>
-            <select name="">
-                <option value="8">8</option>
-                <option value="10">10</option>
+        <?php
+            $nbreJoueurs = $_GET['nbreJoueurs'];
+           if(!isset($nbreJoueurs)){
+               ?>
+               <form method="get" action="nouveauTournoi.php">
+                <label for="nbreJoueurs">Nombre de participants au tournoi :</label>
+                <select name="nbreJoueurs">
+                <option value="8" selected>8 joueurs</option>
+                <option value="10">10 joueurs</option>
+                </select>
+                <input type="submit" value="Valider"/>
+                   <?php
+           }
+           else if(isset($nbreJoueurs AND (($nbreJoueurs ==8)OR ($nbreJoueurs==10))){
 
-            </select>
+               for($i =1; $i <= $nbreJoueurs; $i++){
+                    ?>
+               }
+
+           }
+
+
+        ?>
         </form>
     </div>
 </div>
