d07bf8fa63255787441ce218d67e3b3ac5f071fd
aurelien-blot <aurelien.blot@gmail.com>
2018-01-19 09:18:55 +0100

    php + cahampionnat

--- include/navChamp.php
+++ include/navChamp.php
@@ -1,9 +1,8 @@
 
 <?php include'include/menuPrincipal.php' ?>
 <ul>
-    <li><a></a></li>
-    <li><a></a></li>
-    <li><a></a></li>
-    <li><a></a></li>
+    <li><a href="championnat.php?encours=1">En cours</a></li>
+    <li><a href="nouveauTournoi.php">Nouveau Tournoi</a></li>
+    <li><a href="championnat.php?histo=1">Historique</a></li>
 </ul>
 
--- joueurs.php
+++ joueurs.php
@@ -1,6 +1,8 @@
 <?php session_start();
 include'include/connexionBdd.php';
 $listeJoueurs = $bdd->query('SELECT * FROM joueurs ORDER BY nom');
+$detailJoueur = $bdd->prepare('SELECT * FROM joueurs WHERE id_Joueur =?');
+$detailJoueur->execute(array($_GET['idJ']));
 ?>
 
 <!doctype html>
@@ -27,9 +29,24 @@ $listeJoueurs = $bdd->query('SELECT * FROM joueurs ORDER BY nom');
         if(!isset($_GET['idJ'])) {
             while ($donnees = $listeJoueurs->fetch()) {
                 ?>
+                <div class="liendetailJoueur">
                 <a href="joueurs.php?idJ=<?php echo $donnees['id_Joueur']; ?>"><img id="photoJoueur"
                                                                                     src="<?php echo $donnees['photo']; ?>"></a>
                 <p id="nomJoueur"><?php echo $donnees['surnom']; ?></p>
+                </div>
+                <?php
+            }
+        }
+        else{
+            while ($donnees =  $detailJoueur->fetch()){
+                ?>
+                <h2 id="surnomJoueur"><?php echo $donnees['surnom']; ?></h2>
+                <img src="<?php echo $donnees['photo']; ?>">
+                <h3 id="nomCompletJoueur"><?php echo $donnees['nom']; ?> <?php echo $donnees['prenom']; ?></h3>
+                <h3>Championnats remportés : <?php echo $donnees['victoireChamp']; ?></h3>
+                <p>Qualité : <?php echo $donnees['qualité']; ?></p>
+                <p>Défaut : <?php echo $donnees['defaut']; ?></p>
+                <p>Meilleure performance : <?php echo $donnees['meilleureperformance']; ?></p>
                 <?php
             }
         }
