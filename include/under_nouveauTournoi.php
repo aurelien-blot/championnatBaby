7b62f8cec79a7fb1d24f076485956d237d0fc611
aurelien-blot <aurelien.blot@gmail.com>
2018-01-23 10:25:42 +0100

    equipes aléatoires ok, début de la création des matchs

--- include/under_nouveauTournoi.php
+++ include/under_nouveauTournoi.php
@@ -1,3 +1,156 @@
 <?php
+include 'connexionBdd.php';
+
+$nbreJoueurs = intval($_GET['nbreJoueurs']);
+$insertCompet = $bdd->prepare('INSERT INTO competitions(nomChamp, nbreJoueurs, dateDebut) VALUES(:nomChamp, :nbreJoueurs, :dateDebut)');
+$insertCompet->execute(array(
+    'nomChamp' => $_POST['nomChamp'],
+    'nbreJoueurs' => $nbreJoueurs,
+    'dateDebut' => '20080101'
+));//$_POST['dateDebut']
+$insertCompet->closeCursor();
+$reqIdC = $bdd->prepare('SELECT * FROM competitions WHERE nomChamp = ?');
+$nbreParticipants = 8;
+
+$listeEquipes = array();
+
+$listeJoueurs3 = array();
+/* TEST
+$listeJoueurs3[]=$_POST['joueur1'];
+$listeJoueurs3[]=$_POST['joueur2'];
+$listeJoueurs3[]=$_POST['joueur3'];
+$listeJoueurs3[]=$_POST['joueur4'];
+$listeJoueurs3[]=$_POST['joueur5'];
+$listeJoueurs3[]=$_POST['joueur6'];
+$listeJoueurs3[]=$_POST['joueur7'];
+$listeJoueurs3[]=$_POST['joueur8'];
+*/
+$listeJoueurs3[]=1;
+$listeJoueurs3[]=2;
+$listeJoueurs3[]=3;
+$listeJoueurs3[]=4;
+$listeJoueurs3[]=5;
+$listeJoueurs3[]=6;
+$listeJoueurs3[]=7;
+$listeJoueurs3[]=8;
+// AFFICHER LISTE ID JOUEURS
+/*
+?>
+<p>Liste des joueurs :</p><br><ul><?php
+foreach ($listeJoueurs3 as $jtest){
+    ?>
+    <li><?php echo($jtest);?></li>
+    <?php
+}
+    ?> </ul><?php*/
+// FIN TEST
+
+// CREATION DES EQUIPES !!
+$compteurEquipe =1;
+$joueurcase = array();
+for($i =0; $i<count($listeJoueurs3);$i++){
+//for($compteurEquipe =1; $compteurEquipe<=4;$compteurEquipe++){
+//while(count($listeJoueurs3)>=2){
+    //$i=0;
+    $joueur = $listeJoueurs3[$i];
+    $equipe=array();
+    $equipe['idE']=$compteurEquipe;
+    $equipe['joueur1']=$joueur;
+    /*
+    ?>
+    <p>Equipe : <?php echo($equipe['idE'])?></p>
+    <p>Joueur 1 : <?php echo($equipe['joueur1'])?></p>
+    <?php*/
+    unset($listeJoueurs3[$i]);
+    $listeJoueurs3 = array_values($listeJoueurs3);
+    $rand = $i;
+    while($rand == $i){
+        $rand = rand(0,(count($listeJoueurs3)-1));
+    }
+    $equipe['joueur2'] = $listeJoueurs3[$rand];
+    /*
+    ?>
+    <p>RAND : <?php echo($rand);?></p><?php
+
+    ?>
+    <p>Joueur 2 : <?php echo($equipe['joueur2']);?></p><?php
+    */
+    unset($listeJoueurs3[$rand]);
+    $listeJoueurs3 = array_values($listeJoueurs3);
+
+
+/*
+    ?>
+    <p>Liste des joueurs restants :</p><br><ul><?php
+    foreach ($listeJoueurs3 as $jtest){
+        ?>
+        <li><?php echo($jtest);?></li>
+        <?php
+    }
+    ?> </ul><?php*/
+    $listeEquipes[]=$equipe;
+    $compteurEquipe++;
+}
+$equipe["idE"]=$compteurEquipe;
+$equipe['joueur1']=$listeJoueurs3[0];
+$equipe['joueur2']=$listeJoueurs3[1];
+$listeEquipes[]=$equipe;
+
+// FIN DE CREATION DES EQUIPES
+
+foreach ($listeEquipes as $equipe){
+    echo ("Equipe : ".$equipe['idE']." Joueur 1 :".$equipe['joueur1']." / Joueur 2 : ".$equipe['joueur2']);
+    ?><br><?php
+}
+
+//ENTREE DES EQUIPES DANS LA BDD;
+
+$reqIdC->execute(array($_POST['nomChamp'] ));
+
+$compet;
+
+while($donnees = $reqIdC->fetch()){
+    $compet = $donnees['id_competition'];
+    //echo($donnees['nomChamp']);
+}
+$reqIdC->closeCursor();
+
+ $reqPushEC= $bdd->prepare('INSERT INTO equipes(joueur1, joueur2, id_compet) VALUES(:joueur1, :joueur2, :id_compet)');
+
+foreach ($listeEquipes as $equipe) {
+    $reqPushEC->execute(array(
+        'joueur1' => $equipe['joueur1'],
+        'joueur2' => $equipe['joueur2'],
+        'id_compet' => $compet
+    ));
+}
+$reqPushEC->closeCursor();
+
+// CA MARCHE
+
+
+$reqEC = $bdd->prepare('SELECT * FROM equipes WHERE id_compet = ?');
+$reqEC->execute(array($compet));
+
+$listeEquipes2 = array();
+
+while($donnees = $reqEC->fetch()){
+    $listeEquipes2[]=$donnees['id_Equipe'];
+}
+$reqEC->closeCursor();
+$reqMatchsE = $bdd->prepare('INSERT INTO matchs (equipe1, equipe2, id_compet) VALUES (:equipe1, :equipe2, :id_compet)');
+
+for($o=0;$o<=2;$o++){
+    $equipe1 = $listeEquipes2[o];
+    $reqMatchsE->execute(array(
+        'equipe1' => $equipe1,
+        'equipe2' => $equipe2,
+        'id_compet' => $compet));
+}
+
+
+
+
+
 
 ?>
--- nouveauTournoi.php
+++ nouveauTournoi.php
@@ -1,5 +1,6 @@
 <?php session_start();
 include'include/connexionBdd.php';
+$listeJoueurs2 = $bdd->query('SELECT * FROM joueurs ORDER BY nom');
 ?>
 
 <!doctype html>
@@ -22,36 +23,63 @@ include'include/connexionBdd.php';
             <?php include'include/navChamp.php'; ?>
         </nav>
         <?php
-            $nbreJoueurs = $_GET['nbreJoueurs'];
-           if(!isset($nbreJoueurs)){
-        ?>
-        <form method="get" action="nouveauTournoi.php">
-            <label for="nbreJoueurs">Nombre de participants au tournoi :</label>
-            <select name="nbreJoueurs">
-                <option value="8" selected>8 joueurs</option>
-                <option value="10">10 joueurs</option>
-            </select>
-            <input type="submit" value="Valider"/>
-            <?php
-            }
-           else if(isset($nbreJoueurs AND (($nbreJoueurs ==8)OR ($nbreJoueurs==10))){
-                ?>
 
+           if(!isset($_GET['nbreJoueurs'])) {
+               ?>
+               <form method="get" action="nouveauTournoi.php">
+               <label for="nbreJoueurs">Nombre de participants au tournoi :</label>
+               <select name="nbreJoueurs">
+                   <option value="8" selected>8 joueurs</option>
+                   <option value="10">10 joueurs</option>
+               </select>
+               <input type="submit" value="Valider"/>
+               </form>
                <?php
-               for($i =1; $i <= $nbreJoueurs; $i++){
+           }
+
+           else if(isset($_GET['nbreJoueurs']) AND ($_GET['nbreJoueurs'] ==8 OR $_GET['nbreJoueurs']==10)){
+               $nbre = $_GET['nbreJoueurs'];
+
+
+               ?>
+                <form method="post" action="include/under_nouveauTournoi.php?nbreJoueurs=<?php echo($_GET['nbreJoueurs'])?>">
+                    <label for="nomChamp">Nom du tournoi : </label>
+                    <input name="nomChamp" type="text" required>
+                    <label for="dateDebut">Date de début du tournoi :</label>
+                    <input type="date" name="dateDebut" required>
+                    <p>Joueurs participant au tournoi :</p>
+               <?php
+
+               while($donnees = $listeJoueurs2->fetch()){
+
+                   $detailJoueur['id']= $donnees['id_Joueur'];
+                   $detailJoueur['prenom']= $donnees['prenom'];
+                   $tableauJoueur[]=$detailJoueur;
+
+               }
+               for($i =1; $i <= $_GET['nbreJoueurs']; $i++){
                     ?>
                    <label for="joueur<?php echo($i);?>">Joueur <?php echo($i);?> :</label>
                    <select name="joueur<?php echo($i);?>">
-
+                    <?php
+                    foreach ($tableauJoueur as $joueur) {
+                        ?>
+                        <option value="<?php echo($joueur['id']);?>"><?php echo($joueur['prenom']); ?></option>
+                        <?php
+                    }
+                    ?>
                    </select>
                    <?php
                }
-
+               ?>
+               <input type="submit" value="Valider">
+                </form>
+               <?php
            }
 
 
         ?>
-        </form>
+
     </div>
 </div>
 <footer>
